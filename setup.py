6a092f794d95deea895ce59781dfd78d2929cdec
Khairul Imam <ki65559@gmail.com>
2019-08-03 15:07:58 +0800

    change package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 setuptools.setup(
-    name='facenet-khairulimam',
+    name='res-facenet',
     version='0.0.1',
     author='Khairul Imam',
     author_email='ki65559@gmail.com',
