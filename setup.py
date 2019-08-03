1e7a0c1341f68bcec5061b231d5677c95f0758ad
Khairul Imam <ki65559@gmail.com>
2019-08-03 18:44:44 +0800

    cleaning init file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='res-facenet',
-    version='0.0.3',
+    version='0.0.4',
     author='Khairul Imam',
     author_email='ki65559@gmail.com',
     description='This model has been trained on VGGv2 and tested on LFW with 92% accuracy. The pretrained model can be accessed on res_facenet.model_921 or res_facenet.model_920',
